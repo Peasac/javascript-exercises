582e93bbf57631d384acb294391c2ebbf637b853
Peasac <ayaantabrez.10@gmail.com>
2024-10-14 18:59:19 +0530

    Completed 02_repeatString assignment by TOP.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let newString = "";
+    for(let i=1;i<=num;i++) {
+         newString = newString.concat(string);
+    }
+    return newString;
 
 };
 
