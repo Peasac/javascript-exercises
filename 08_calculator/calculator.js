8868af31858b255450f024b62f03d6d345dd7d93
Peasac <ayaantabrez.10@gmail.com>
2024-12-05 20:34:45 +0530

    Completed 08_Calculator assignment

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const summ = arr.reduce((total, current)=> total+current, 0);
+  return summ
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, current)=> total*current, 1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let c = 1;
+	for(let i=1;i<=b;i++)
+  {
+    c= c*a;
+  }
+  return c;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let fact = 1;
+  for(let i=a;i>0;i--)
+  { 
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
