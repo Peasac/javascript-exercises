875b550dda295d26c94bdb23b13c4d1b9585b46f
Peasac <ayaantabrez.10@gmail.com>
2024-12-06 23:11:59 +0530

    Completed 09_palindromes assignment

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = (a) => {
+    let ab = a.toLowerCase();
+    ab = Array.from(ab).filter((letter) => /[a-zA-Z0-9]/.test(letter)).join("");
+    console.log(ab);
+    let rev = Array.from(ab).reverse().join("");
+    console.log(rev);
+    return rev === ab;
+}
 
-};
+
+    
+
 
 // Do not edit below this line
 module.exports = palindromes;
