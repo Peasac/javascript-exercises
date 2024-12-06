47536c70244b1a20dc95758a014ee019507e35f0
Peasac <ayaantabrez.10@gmail.com>
2024-12-06 23:53:40 +0530

    Completed 10_fibonacci and 11_getTheTitles

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let count;
+    if (typeof num !== 'number') {
+        count = parseInt(num);
+    } else {
+        count = num;
+    }
 
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+
+    let num1 = 1;
+    let num2 = 0;
+
+    for (let i = 2; i <= count; i++) {
+        let current = num1 + num2;
+        num2= num1;
+        num1 = current;
+    }
+
+    return num1;
 };
 
 // Do not edit below this line
