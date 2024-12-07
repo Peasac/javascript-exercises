404dbbe094bd4657f725423e92f7c710d17ddda5
Peasac <ayaantabrez.10@gmail.com>
2024-12-07 10:38:31 +0530

    Finished 12_findTheOldest assignment

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+function getAge(birth,death) {
+    if(!death) {
+        death = new Date().getFullYear();
+        return death - birth;
+    }
+    else return death-birth;
+}
 
+const findTheOldest = function(people) {
+    Oldest = people.reduce((oldest, current)=> {
+        let oldest1 = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let current1 = getAge(current.yearOfBirth, current.yearOfDeath);
+        return oldest1>current1?oldest:current;
+    })
+    return  Oldest;
+    
 };
 
 // Do not edit below this line
